970755eaa08bbd0ad3aa6165acb0913d86f8d585
Suman Kumar Mishra <sumanmishra33330@gmail.com>
2023-04-22 11:31:16 +0530

    new line added grfgffhjyj

---
+++ new.py
@@ -0,0 +1,75 @@
+
+# First="Toney"
+# Last="Stark"
+# age = 34
+# Genious=True
+# print(First,Last,age,Genious)
+# name= input("what is your namme?")
+# print(name)
+# Old_name=input("Enter youe old name?")
+# new_name=int(Old_name) + 5
+# # print(new_name)
+# name="Suman Kumar Mishra"
+# age=input("enter age")
+# if int(age) < 20 :
+#    print("you ar child")
+
+# elif int(age) >20 and int(age) <60 :
+#    print("you in collage")
+# else:
+#    print("you are a old man")
+# age=40
+# if age > 29 :
+#     print("adult")
+# else:
+#     print("you are a child")
+# marks=['ram' ,'same' , 'jodu','modhu']
+# marks.clear()
+# for mark in marks:
+#     if mark =='jodu':
+#        continue;
+#     print(mark)
+# import math
+# print(math.gcd(3,6))
+# a=5
+# b=4
+# print(a&b)
+
+
+
+# input=input("enter valu:   ")
+# if input == 'A' or "a":
+#     print(input," is vowal")
+# elif input =='E' :
+#     print(input," is vowal")
+# elif input == 'I':
+#     print(input," is vowal")
+# elif input =='O':
+#     print(input," is vowal")
+# elif input == 'U':
+#     print(input," is vowal")
+# elif input == 'a' :
+#     print(input," is vowal")
+# elif input =='e' :
+#     print(input," is vowal")
+# elif input == 'i':
+#     print(input," is vowal")
+# elif input =='o':
+#     print(input," is vowal")
+# elif input == 'u':
+#     print(input," is vowal")
+# else:
+#     print(input," is consonent")
+
+
+
+for n in range(6):
+    print(n)
+    print("")
+n=input("enter")
+n=int(n)
+for i in range(0,n):
+    for j in range(1,i+1):
+        print( j ,end=" ")
+    print("\n")
+
